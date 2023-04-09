5f76d03cfae40d3a7d77aa82faf0c91ca7236bbe
hesblac <olaiyagbolagade@gmail.com>
2023-04-09 06:06:24 +0100

    new python

--- hello.py
+++ hello.py
@@ -1,36 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        return render_template('welcome.html', name=name)
-    else:
-        return render_template('index.html')
+@app.route('/')
+def home():
+    return 'Got any creative ideas for a 10 year old’s birthday continue the groovy script by adding a deleting stage for the images after creation tested and it all deploy according to jenkins pipeline'
 
 if __name__ == '__main__':
     app.run()
-
-
-<!-- templates/index.html -->
-<!doctype html>
-<html>
-  <body>
-    <form action="/" method="post">
-      <label for="name">Enter your name:</label>
-      <input type="text" id="name" name="name">
-      <button type="submit">Submit</button>
-    </form>
-  </body>
-</html>
-
-<!-- templates/welcome.html -->
-<!doctype html>
-<html>
-  <body>
-    <h1>Welcome {{ name }}!</h1>
-  </body>
-</html>
-
